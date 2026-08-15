43d6eb560f2b33e156115d23664708e3ef8848bf
sunjaewow <chltjswo789@naver.com>
2026-08-15 16:16:21 +0900

    chore : first settings

---
+++ src/main/java/com/highconcurrency/ticketing/HighConcurrencyTicketingApplication.java
@@ -0,0 +1,12 @@
+package com.highconcurrency.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrencyTicketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighConcurrencyTicketingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/highconcurrency/ticketing/HighConcurrencyTicketingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.highconcurrency.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrencyTicketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
