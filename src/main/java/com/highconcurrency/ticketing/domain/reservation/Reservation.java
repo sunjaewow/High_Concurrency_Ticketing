32e3834e52781810710219096cf1683d90dfdbf1
sunjaewow <chltjswo789@naver.com>
2026-08-17 19:53:29 +0900

    feat : reservation 사용자, 콘서트 유니크 제약조건 추가

--- src/main/java/com/highconcurrency/ticketing/domain/reservation/Reservation.java
+++ src/main/java/com/highconcurrency/ticketing/domain/reservation/Reservation.java
@@ -3,17 +3,14 @@ package com.highconcurrency.ticketing.domain.reservation;
 import com.highconcurrency.ticketing.domain.BaseEntity;
 import com.highconcurrency.ticketing.domain.concert.Concert;
 import com.highconcurrency.ticketing.domain.user.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "reservations")
+@Table(name = "reservations", uniqueConstraints = @UniqueConstraint(name = "uk_reservations_user_id_concert_id", columnNames = {"user_id", "concert_id"}))
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
