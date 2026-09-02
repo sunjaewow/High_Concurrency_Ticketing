0a41d1ba20548b19b93dd3015e7cb696ab2f3aef
sunjaewow <chltjswo789@naver.com>
2026-09-02 15:55:30 +0900

    test : ec2 ip 환경변수 설정

--- loadtest/reservation-queue-load-test.js
+++ loadtest/reservation-queue-load-test.js
@@ -6,6 +6,7 @@ const CONCERT_ID = 1;
 const NODE_INDEX = Number(__ENV.K6_NODE_INDEX || 0);
 const USERS_PER_NODE = Number(__ENV.USERS_PER_NODE || 25000);
 const START_AT = Number(__ENV.START_AT || 0);
+const EC2_IP = String(__ENV.EC2_IP || 'localhost');
 
 export const options = {
     scenarios: {
@@ -29,7 +30,7 @@ export default function () {
     waitUntilStartAt();
 
     const userId = NODE_INDEX * USERS_PER_NODE + exec.scenario.iterationInTest + 1;
-    const url = `http://54.116.140.111:8080/reservation-queues/${CONCERT_ID}/users/${userId}`;
+    const url = `http://${EC2_IP}:8080/reservation-queues/${CONCERT_ID}/users/${userId}`;
 
     const response = http.post(url, null, {
         tags: {
